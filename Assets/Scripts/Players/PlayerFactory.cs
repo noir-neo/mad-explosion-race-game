06e6bf160614ff4b18aa837709cc93e4bb3891f3
Shota HAMADA <maku693@outlook.com>
2018-04-29 20:23:29 +0900

    Make the vehicles generated in the starting grid

--- Assets/Scripts/Players/PlayerFactory.cs
+++ Assets/Scripts/Players/PlayerFactory.cs
@@ -1,17 +1,32 @@
 using System.Collections.Generic;
+using System.Linq;
+using System;
 using AIs;
 using Cinemachine;
 using Players.InputImpls;
 using UnityEngine;
 using Zenject;
+using Random = UnityEngine.Random;
 
 namespace Players
 {
     class PlayerFactory : MonoBehaviour
     {
         [SerializeField] private List<GameObject> machines;
+        [SerializeField] private Vector2Int _gridSize;
+        [SerializeField] private Vector3 _firstGridPosition;
+        [SerializeField] private Vector2 _gridInterval;
 
         [Inject] private CoursePath coursePath;
+
+        [Inject] private GameManagers.IMainGameStarter _mainGameStarter;
+
+        private List<PlayerCore> players;
+
+        void Start()
+        {
+            players = CreatePlayers();
+        }
 
         public PlayerCore CreateHuman(int inputId)
         {
@@ -34,6 +49,22 @@ namespace Players
         private PlayerCore CreatePlayer()
         {
             return CreatePlayer(Vector3.zero, Quaternion.identity);
+        }
+
+        private List<PlayerCore> CreatePlayers()
+        {
+            return Enumerable.Range(0, _gridSize.x)
+                .SelectMany(x => {
+                    return Enumerable.Range(0, _gridSize.y)
+                        .Select(y => new Vector2(x, y));
+                })
+                .Select(grid => {
+                    var offset = _gridInterval.X0Y();
+                    offset.Scale(grid.X0Y());
+                    var position = _firstGridPosition + offset;
+                    return CreatePlayer(position, Quaternion.identity);
+                })
+                .ToList();
         }
 
         private PlayerCore CreatePlayer(Vector3 position, Quaternion rotation)
