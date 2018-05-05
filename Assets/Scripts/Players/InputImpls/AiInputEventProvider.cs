193752e559f68069d22099a1b76ce9b214b49de5
Shoma SATO <noir.neo.04@gmail.com>
2018-05-05 18:46:40 +0900

    Faster

--- Assets/Scripts/Players/InputImpls/AiInputEventProvider.cs
+++ Assets/Scripts/Players/InputImpls/AiInputEventProvider.cs
@@ -11,6 +11,8 @@ namespace Players.InputImpls
     public class AiInputEventProvider : MonoBehaviour, IInputEventProvider
     {
         private readonly ISubject<CoursePath> _coursePath = new AsyncSubject<CoursePath>();
+        private readonly ReactiveProperty<float> _dotNextCorner = new ReactiveProperty<float>();
+
         [SerializeField] Vector3ReactiveProperty _nextCorner = new Vector3ReactiveProperty();
 
         void Start()
@@ -18,9 +20,18 @@ namespace Players.InputImpls
             _coursePath.Select(path => path.Corners.ToList())
                 .SelectMany(corners => this.FixedUpdateAsObservable(), (x, _) => x)
                 .WithLatestFrom(_nextCorner, Tuple.Create)
-                .Where(t => Vector3.Distance(transform.position, t.Item2) < 50)
+                .Where(t => Vector3.Distance(transform.position, t.Item2) < 60)
                 .Select(t => t.Item1.ElementAt((t.Item1.IndexOf(t.Item2) + 1) % t.Item1.Count))
                 .Subscribe(x => _nextCorner.Value = x)
+                .AddTo(this);
+
+            this.FixedUpdateAsObservable()
+                .WithLatestFrom(_nextCorner, (_, x) =>
+                {
+                    var targetDir = x - transform.position;
+                    return Vector3.Dot(transform.right.normalized, targetDir.normalized);
+                })
+                .Subscribe(x => _dotNextCorner.Value = x)
                 .AddTo(this);
         }
 
@@ -33,29 +44,29 @@ namespace Players.InputImpls
         IObservable<bool> IInputEventProvider.GetAccelAsObservable()
         {
             return Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(500))
-                .Select(_ => Random.Range(0, 10) > 0)
+                .WithLatestFrom(_dotNextCorner, (_, x) => x)
+                .Select(Mathf.Abs)
+                .Select(x => x < Random.Range(0.3f, 0.5f))
                 .DistinctUntilChanged();
         }
 
         IObservable<float> IInputEventProvider.GetSteeringAsObservable()
         {
             return Observable.Timer(TimeSpan.FromSeconds(0), TimeSpan.FromMilliseconds(50))
-                .WithLatestFrom(_nextCorner, (_, x) => x)
+                .WithLatestFrom(_dotNextCorner, (_, x) => x)
                 .Select(GetXAxisInput)
                 .DistinctUntilChanged();
         }
 
-        private float GetXAxisInput(Vector3 target)
+        private float GetXAxisInput(float dot)
         {
-            var targetDir = target - transform.position;
-            var dot = Vector3.Dot(transform.right.normalized, targetDir.normalized);
             if (Mathf.Abs(dot) < Random.Range(0f, 0.2f)) return 0;
             return dot;
         }
 
         void OnDrawGizmosSelected()
         {
-            Gizmos.DrawSphere(_nextCorner.Value, 50);
+            Gizmos.DrawSphere(_nextCorner.Value, 60);
         }
     }
 }
